e45c68f195f8b3db35edbd38712a6edad253e82a
Reza.m98u <reza137998@gmail.com>
2025-08-24 14:01:14 +0100

    new build

--- Toolidol.Api/obj/Debug/net8.0/Toolidol.Api.AssemblyInfo.cs
+++ Toolidol.Api/obj/Debug/net8.0/Toolidol.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toolidol.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b370492ec179001c402d78c9ec1114d586fe72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toolidol.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toolidol.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
